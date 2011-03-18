caf98df3964fd511826f5d137282995eed57768e
jcsodelta@gmail.com <jcsodelta@gmail.com@1a9bb392-2921-7654-099c-25bc72623dd4>
2011-03-18 16:01:29 +0000

    Classe CARTA ok Revendo classe BARALHO

--- trunk/pilha.h
+++ trunk/pilha.h
@@ -1,13 +1,1 @@
-using namespace std;
-
-class PILHA{
-private:
-	CARTA cartas[52];
-	int tamanhoMax;
-	int tamanhoAtual;
-public:
-	PILHA();
-	int getTamanho();
-	CARTA getCarta( int );
-	bool add( CARTA );
-}; 
+ 
--- trunk/yukon.cpp
+++ trunk/yukon.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
 #include "carta.h"
 #include "baralho.h"
-#include "pilha.h"
 
 using namespace std;
 
-// sobrecarga da impressão
 ostream &operator<<(ostream & out, CARTA c ){
 	// imprime de acordo cm a visibilidade
 	if(c.getVisivel())
@@ -18,42 +14,15 @@ ostream &operator<<(ostream & out, CARTA c ){
 	return out;
 }
 
-ostream &operator<<( ostream & out, PILHA p){
-	for(int carta=0; carta<p.getTamanho(); carta++)
-		out << p.getCarta(carta) << endl;
+
+int main (){
 	
-	return out;
-}
-
-//== MAIN ===========================================
-
-int main(){
-
-	BARALHO baralho;
-	PILHA pilhas[7];
-
-	CARTA c;
-	c = baralho.getCartaRand();
-	c.setVisivel(true);
-	pilhas[0].add(c);
-	cout << pilhas[0];
-
-	for(int pilha=1; pilha<7; pilha++){
-		int p;
-		for(p=0; p<pilha; p++){
-			c = baralho.getCartaRand();
-			c.setVisivel(false);
-			pilhas[pilha].add(c);
-		}
-		cout << "ops\n";
-		for(p=0; p<5; p++){
-			c = baralho.getCartaRand();
-			c.setVisivel(true);
-			pilhas[pilha].add(c);
-		}
-
-		cout << pilhas[pilha];
-	}
-
+	BARALHO b;
+	CARTA a = b.getCartaRand();
+	cout << a << endl;
+	
+	a.setVisivel(true);
+	cout << a << endl;
+	
 	return 0;
 }
