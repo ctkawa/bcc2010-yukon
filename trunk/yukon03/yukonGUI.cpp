79267c8add70dac9e6169d6e1c72fa356b9f8a64
jcsodelta@gmail.com <jcsodelta@gmail.com@1a9bb392-2921-7654-099c-25bc72623dd4>
2011-03-27 22:02:06 +0000

    Mudanças na GUI >> edit(CentralWidget)

--- trunk/yukon03/yukonGUI.cpp
+++ trunk/yukon03/yukonGUI.cpp
@@ -52,8 +52,36 @@ void YUKON_GUI::status(){
 }
 
 void YUKON_GUI::principal(){
-	QLabel * texto = new QLabel("Ola, ED!");
-	window->setCentralWidget(texto);
+	
+	QWidget * principal = new QWidget();
+	window->setCentralWidget(principal);
+	
+	QHBoxLayout * hbox = new QHBoxLayout();
+	principal->setLayout(hbox);
+	
+	QLabel * texto1 = new QLabel("Monte 1");
+	hbox->addWidget(texto1);
+	
+	QLabel * texto2 = new QLabel("Monte 2");
+	hbox->addWidget(texto2);
+	
+	QLabel * texto3 = new QLabel("Monte 3");
+	hbox->addWidget(texto3);
+	
+	QLabel * texto4 = new QLabel("Monte 4");
+	hbox->addWidget(texto4);
+	
+	QLabel * texto5 = new QLabel("Monte 5");
+	hbox->addWidget(texto5);
+	
+	QLabel * texto6 = new QLabel("Monte 6");
+	hbox->addWidget(texto6);
+	
+	QLabel * texto7 = new QLabel("Monte 7");
+	hbox->addWidget(texto7);
+	
+	QLabel * texto8 = new QLabel("Fundacao");
+	hbox->addWidget(texto8);
 }
 
 void YUKON_GUI::coisa(){
