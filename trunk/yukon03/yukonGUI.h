b08ba54553432f424b67fc4f4869a51b2b2f5b35
jcsodelta@gmail.com <jcsodelta@gmail.com@1a9bb392-2921-7654-099c-25bc72623dd4>
2011-03-27 21:26:02 +0000

    Mudanças na GUI >> add(Menubar) >> add(ToolBar) >> add(StatusBar)

--- trunk/yukon03/yukonGUI.h
+++ trunk/yukon03/yukonGUI.h
@@ -1,13 +1,23 @@
 #include <yukon.h>
 
+#include <QObject>
 #include <QApplication>
 #include <QMainWindow>
+#include <qmenubar.h>
+#include <QToolBar>
+#include <qstatusbar.h>
 
-class YUKON_GUI : public YUKON {
+class YUKON_GUI : public YUKON, public QObject {
 private:
 	QApplication * app;
 	QMainWindow * window;
+	void menu();
+	void toolbar();
+	void status();
 public:
 	YUKON_GUI();
 	int run(int, char **);
+	
+public slots:
+	void coisa(); 
 };
