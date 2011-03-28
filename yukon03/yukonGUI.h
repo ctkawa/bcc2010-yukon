172a4f0f62f4c29f283018996b2f266e4a60e7f3
jcsodelta <jcsodelta@gmail.com>
2011-03-28 04:28:29 +0000

    Its Alive! >> edit(CentralWidget)

--- yukon03/yukonGUI.h
+++ yukon03/yukonGUI.h
@@ -10,9 +10,9 @@
 #include <QWidget>
 #include <QBoxLayout>
 #include <QLabel>
+#include <QMouseEvent>
 
 class YUKON_GUI : public YUKON, public QObject {
-	
 private:
 	QApplication * app;
 	QMainWindow * window;
@@ -21,10 +21,30 @@ private:
 	void status();
 	void principal();
 	QLabel * getImagemCarta(QWidget*, CARTA);
+	
+	int deMonte;
+	int deCarta;
+	
 public:
 	YUKON_GUI();
 	int run(int, char **);
 	
+	bool isSetDe();
+	void setDe(int, int);
+	void limpaDe();
+	void movimenta(int);
+	
 public slots:
 	void coisa(); 
+};
+
+class GUI_CARTA : public QLabel {
+private:
+	YUKON_GUI * yukon;
+	int monte;
+	int ordem;
+public:
+	GUI_CARTA(YUKON_GUI *, QWidget*, int, int);
+	void setCarta(CARTA);
+	void mouseReleaseEvent( QMouseEvent * );
 };
